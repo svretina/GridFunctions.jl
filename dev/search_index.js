fa5644e0769029f48439ce4d467d036321cf46a9
Documenter.jl <documenter@juliadocs.github.io>
2023-08-22 15:31:39 +0000

    build based on 18c6123

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GridFunctions","category":"page"},{"location":"#GridFunctions","page":"Home","title":"GridFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GridFunctions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GridFunctions]","category":"page"}]
+}
